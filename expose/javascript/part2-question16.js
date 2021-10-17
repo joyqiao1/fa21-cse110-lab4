0a867ba70ad4c756f81d3890471a2f97597092dc
joyqiao1 <91338627+joyqiao1@users.noreply.github.com>
2021-10-16 17:45:15 -0700

    Add files via upload

---
+++ expose/javascript/part2-question16.js
@@ -0,0 +1,11 @@
+const statistics = {
+    redCars: 21,
+    blueCars: 45,
+    greenCars: 12,
+    raceCars: 5,
+    blackCars: 40,
+    rareCars: 2
+};
+for (const property in statistics){
+    console.log(`${property}: ${statistics[property]}`);
+}
